2a804bbcad541a9d21e21898a40d1cbd4212bd79
faliberti <69914057+faliberti@users.noreply.github.com>
2021-03-08 23:40:22 -0500

    Finished code
    Finished the code for the App

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -14,6 +14,8 @@ def to_usd(my_price):
 
 API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY", "abc123")
 
+present = datetime.now()
+
 symbol = input("Please input the symbol of stock(s) (e.g. TSLA).")
 if symbol.isalpha() == False or len(symbol) > 5:
     print("There was an error with what you put. Please try again with a stock symbol. (e.g. TSLA)")
@@ -24,34 +26,55 @@ if "Error Message" in response.text:
     print("Sorry there was an error and we found no trading data for that symbol.")
     exit()
 parsed_response = json.loads(response.text)
-def transform_response(parsed_response):
-    trans = parsed_response['Time Series (Daily)']
-    rows = []
-    for date, daily_prices in trans.items():
-        row = {
-            'timestamp': date,
-            "open price": float(daily_prices["1. open"]),
-            "high": float(daily_prices["2. high"]),
-            "low": float(daily_prices["3. low"]),
-            "close price": float(daily_prices["4. close"]),
-            "volume": int(daily_prices["5. volume"])
-        }
-        rows.append(row)
 
-    return rows
+tsd = parsed_response["Time Series (Daily)"]
+dates = list(tsd.keys())
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close_price = tsd[dates[0]]["4. close"]
+
+high_price = [float(tsd[date]["2. high"]) for date in dates]
+low_price = [float(tsd[date]["3. low"]) for date in dates]
+
+recent_high_price = max(high_price)
+recent_low_price = min(low_price)
+
+csv_file_path = os.path.join(os.path.dirname(__file__),"..", "data", "prices.csv")
+
+with open(csv_file_path, "w") as csv_file: 
+    writer = csv.DictWriter(csv_file, fieldnames=["timestamp", "open", "high", "low", "close", "volume"])
+    writer.writeheader() 
+    for date in dates:
+        writer.writerow({
+            "timestamp": date, 
+            "open": tsd[date]["1. open"], 
+            "high": tsd[date]["2. high"], 
+            "low": tsd[date]["3. low"], 
+            "close": tsd[date]["4. close"], 
+            "volume": tsd[date]["5. volume"]
+            })
+
+recommend = "Do Not Buy"
+reason = 'The most recent closing price was 25 percent above its most recent low.'
+
+if float(latest_close_price) <= (1.25 * recent_low_price):
+    recommend = "Buy"
+    reason = "The most recent closing price is under 1.25x of the most recent low."
+
+
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL:", symbol.upper())
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT:", present.strftime("%B %d, %Y at %I:%M %p"))
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print("LATEST DAY:", last_refreshed)
+print("LATEST CLOSE", to_usd(float(latest_close_price)))
+print("RECENT HIGH:", to_usd(recent_high_price))
+print("RECENT LOW:", to_usd(recent_low_price))
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
+print("RECOMMENDATION:", recommend)
+print("RECOMMENDATION REASON:", reason)
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
