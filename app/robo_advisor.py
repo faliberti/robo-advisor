79fe42644b96454a667273d227c53872f540d852
faliberti <69914057+faliberti@users.noreply.github.com>
2021-03-08 23:03:03 -0500

    User Input
    writing the code for the user to input what stocks they want to view as well as getting the API info

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -14,6 +14,31 @@ def to_usd(my_price):
 
 API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY", "abc123")
 
+symbol = input("Please input the symbol of stock(s) (e.g. TSLA).")
+if symbol.isalpha() == False or len(symbol) > 5:
+    print("There was an error with what you put. Please try again with a stock symbol. (e.g. TSLA)")
+    exit()
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
+response = requests.get(request_url)
+if "Error Message" in response.text:
+    print("Sorry there was an error and we found no trading data for that symbol.")
+    exit()
+parsed_response = json.loads(response.text)
+def transform_response(parsed_response):
+    trans = parsed_response['Time Series (Daily)']
+    rows = []
+    for date, daily_prices in trans.items():
+        row = {
+            'timestamp': date,
+            "open price": float(daily_prices["1. open"]),
+            "high": float(daily_prices["2. high"]),
+            "low": float(daily_prices["3. low"]),
+            "close price": float(daily_prices["4. close"]),
+            "volume": int(daily_prices["5. volume"])
+        }
+        rows.append(row)
+
+    return rows
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
